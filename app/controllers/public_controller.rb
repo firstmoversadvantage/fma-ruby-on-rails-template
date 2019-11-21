84378b83d71d04ee9fc50994dc67c61c4ea82ae8
oborze <ada.borzemska@polcode.net>
2019-11-21 23:01:15 +0100

    Add terms of use

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,4 +1,8 @@
 class PublicController < ApplicationController
   def index
   end
+  
+  def terms_of_use
+    @title = 'Terms of Use'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'public/index'
+  get 'privacy-policy', to: 'public#privacy_policy'
+  get 'terms-of-use', to: 'public#terms_of_use'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
