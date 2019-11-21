0b97cdbd4d3095d92d8d785df63dbe8b29810746
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2019-11-21 16:18:40 +0100

    Add path to saw_cookie action and displaying flash alerts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'public/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'public#index'
   get 'cookie-policy', to: 'public#cookie_policy'
+  post 'saw_cookie_notice', to: 'public#saw_cookie_notice'
 end
