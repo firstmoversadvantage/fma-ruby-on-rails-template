5d14e60e954e86837afb06eef0f7ce2f75fafb93
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2019-11-27 14:56:59 +0100

    Allow users to recover their password or confirm their account using their username

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,12 +17,16 @@ class User < ApplicationRecord
     @login || self.username
   end
 
-  def self.find_for_database_authentication(warden_conditions)
+  def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
-      where(conditions.to_h).where(["lower(username) = :value", { :value => login.downcase }]).first
-    elsif conditions.has_key?(:username)
-      where(conditions.to_h).first
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      if conditions[:username].nil?
+        where(conditions).first
+      else
+        where(username: conditions[:username]).first
+      end
     end
   end
 end
