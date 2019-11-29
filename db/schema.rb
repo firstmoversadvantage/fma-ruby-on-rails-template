9c2c3ea3516c0ad71a5f6f64b5d0276906da47ca
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2019-11-29 11:56:20 +0100

    Remove email field from db

--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 2019_11_26_122800) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -57,7 +56,6 @@ ActiveRecord::Schema.define(version: 2019_11_26_122800) do
     t.string "email_encrypted"
     t.string "username", null: false
     t.string "email_hash"
-    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
