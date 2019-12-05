749a87d13748eda74ecb15b23f3ad5cac14ca52c
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2019-12-05 15:21:29 +0100

    Add length validations to contact_request name and email_address

--- app/models/contact_request.rb
+++ app/models/contact_request.rb
@@ -1,8 +1,8 @@
 class ContactRequest < ApplicationRecord
   attr_accessor :name, :email_address, :telephone
 
-  validates :name, presence: true
-  validates :email_address, presence: true
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :email_address, presence: true, length: { maximum: 50 }
   validates :comments, length: { maximum: 500 }
 
   include Vault::EncryptedModel
