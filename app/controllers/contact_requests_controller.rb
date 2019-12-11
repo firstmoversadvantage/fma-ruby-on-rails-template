859dd945241e4d65758eca0eeecfbd4f6d785e9b
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2019-12-11 15:27:41 +0100

    Change try to safe operator

--- app/controllers/contact_requests_controller.rb
+++ app/controllers/contact_requests_controller.rb
@@ -2,7 +2,7 @@ class ContactRequestsController < ApplicationController
   before_action :authenticate_user!, only: [:index]
 
   def index
-    if current_user.try(:is_admin?)
+    if current_user&.is_admin?
       @contact_requests = ContactRequest.all
     else
       flash[:warning] = t('public.contact_us.flash.only_admins_allowed')
