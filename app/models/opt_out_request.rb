eb1e2367346e2dc8a06384e7fa5691a966ac20c8
Klaudia Biała <klaudia.biala@polcode.net>
2021-08-05 18:06:58 +0200

    Add validations

--- app/models/opt_out_request.rb
+++ app/models/opt_out_request.rb
@@ -1,6 +1,13 @@
 class OptOutRequest < ApplicationRecord
-  enum request_type: [:opt_out, :disclosure, :delete_data]
+  enum request_type: [:opt_out, :disclosure, :deletion]
 
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :street_address, presence: true, length: { maximum: 50 }
+  validates :city, presence: true, length: { maximum: 50 }
+  validates :postal_code, presence: true, length: { minimum: 5, maximum: 10 }
+  validates :state, presence: true, length: { is: 2 }
+
+  # For attr_encrypted:
   attr_encrypted_options.merge!(
     key: Rails.application.credentials.attribute_key,
     prefix: '',
@@ -10,5 +17,4 @@ class OptOutRequest < ApplicationRecord
   attr_encrypted :city
   attr_encrypted :postal_code
   attr_encrypted :state
-
 end
