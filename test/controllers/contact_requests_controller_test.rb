d9bdb0b62f27f4f73c3d0d295a78cdffebbe1d7b
Klaudia Biala <klaudia.biala@polcode.net>
2021-08-10 18:07:47 +0200

    Better test format

--- test/controllers/contact_requests_controller_test.rb
+++ test/controllers/contact_requests_controller_test.rb
@@ -15,78 +15,83 @@ class ContactRequestsControllerTest < ActionDispatch::IntegrationTest
     }
   end
 
-  test 'should redirect when non admin is logged in' do
-    sign_in @user
-    get contact_requests_path
-    assert_redirected_to root_path + '?locale=en'
-  end
-
-  test 'should get index when admin is logged in' do
-    sign_in @admin
-    get contact_requests_path
-    assert_response :success
-  end
-
-
-  test 'should get contact_us' do
-    get '/contact-us'
-    assert_response :success
-  end
-
-  test 'it creates contact request with correct params' do
-    assert_difference('ContactRequest.count') do
-      post '/contact-us', params: @contact_us_params
+  describe 'not admin user' do
+    test 'should redirect when non admin is logged in' do
+      sign_in @user
+      get contact_requests_path
+      assert_redirected_to root_path + '?locale=en'
     end
-    assert_response :redirect
   end
 
-  test 'do not create contact request with name longer than 20 characters' do
-    @contact_us_params[:contact_request][:name] = Faker::Internet.username(specifier: 21)
-
-    assert_no_difference('ContactRequest.count') do
-      post '/contact-us', params: @contact_us_params
+  describe 'admin' do
+    test 'should get index when admin is logged in' do
+      sign_in @admin
+      get contact_requests_path
       assert_response :success
     end
   end
 
-  test 'it does not create contact request with empty email' do
-    @contact_us_params[:contact_request][:email_address] = ''
-    assert_no_difference('ContactRequest.count') do
-      post '/contact-us', params: @contact_us_params
+  describe 'no user' do
+    test 'should get contact_us' do
+      get '/contact-us'
       assert_response :success
     end
-  end
 
-  test 'do not create contact request with email longer than 50 characters' do
-    @contact_us_params[:contact_request][:email_address] = Faker::Internet.email(
-      name: Faker::Internet.username(specifier: 50),
-      separators: '_'
-    )
-    assert_no_difference('ContactRequest.count') do
-      post '/contact-us', params: @contact_us_params
-      assert_response :success
+    test 'it creates contact request with correct params' do
+      assert_difference('ContactRequest.count') do
+        post '/contact-us', params: @contact_us_params
+      end
+      assert_response :redirect
     end
-  end
 
-  test 'it does not create contact request with email in wrong format' do
-    @contact_us_params[:contact_request][:email_address] = 'wrong_email_format.com'
-    assert_no_difference('ContactRequest.count') do
-      post '/contact-us', params: @contact_us_params
-      assert_response :success
+    test 'do not create contact request with name longer than 20 characters' do
+      @contact_us_params[:contact_request][:name] = Faker::Internet.username(specifier: 21)
+
+      assert_no_difference('ContactRequest.count') do
+        post '/contact-us', params: @contact_us_params
+        assert_response :success
+      end
     end
-  end
 
-  test 'do not create contact request with comments longer than 500 chars' do
-    @contact_us_params[:contact_request][:comments] = Faker::Lorem.characters(number: 501)
-    assert_no_difference('ContactRequest.count') do
-      post '/contact-us', params: @contact_us_params
-      assert_response :success
+    test 'it does not create contact request with empty email' do
+      @contact_us_params[:contact_request][:email_address] = ''
+      assert_no_difference('ContactRequest.count') do
+        post '/contact-us', params: @contact_us_params
+        assert_response :success
+      end
     end
-  end
 
-  test 'should send email to admins' do
-    assert_difference('ActionMailer::Base.deliveries.count') do
-      post '/contact-us', params: @contact_us_params
+    test 'do not create contact request with email longer than 50 characters' do
+      @contact_us_params[:contact_request][:email_address] = Faker::Internet.email(
+        name: Faker::Internet.username(specifier: 50),
+        separators: '_'
+      )
+      assert_no_difference('ContactRequest.count') do
+        post '/contact-us', params: @contact_us_params
+        assert_response :success
+      end
+    end
+
+    test 'it does not create contact request with email in wrong format' do
+      @contact_us_params[:contact_request][:email_address] = 'wrong_email_format.com'
+      assert_no_difference('ContactRequest.count') do
+        post '/contact-us', params: @contact_us_params
+        assert_response :success
+      end
+    end
+
+    test 'do not create contact request with comments longer than 500 chars' do
+      @contact_us_params[:contact_request][:comments] = Faker::Lorem.characters(number: 501)
+      assert_no_difference('ContactRequest.count') do
+        post '/contact-us', params: @contact_us_params
+        assert_response :success
+      end
+    end
+
+    test 'should send email to admins' do
+      assert_difference('ActionMailer::Base.deliveries.count') do
+        post '/contact-us', params: @contact_us_params
+      end
     end
   end
 end
