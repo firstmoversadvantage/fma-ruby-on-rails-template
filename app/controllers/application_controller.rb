2903a5e19e1cc914d21b499d084895f9bfec2952
Tyler VanNurden <tyler.vannurden@colorado.edu>
2021-09-15 09:22:16 -0600

    Get sign up working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  # before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
   around_action :switch_locale
   before_action :check_if_saw_cookie_notice
 
@@ -23,11 +23,11 @@ class ApplicationController < ActionController::Base
     redirect_back(fallback_location: root_path)
   end
 
-  # protected
+  protected
 
-  # def configure_permitted_parameters
-  #   added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
-  #   devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-  #   devise_parameter_sanitizer.permit :account_update, keys: added_attrs
-  # end
+  def configure_permitted_parameters
+    added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
 end
--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -6,9 +6,4 @@ class AdminMailer < ApplicationMailer
       mail(to: admin.email, subject: 'New contact request')
     end
   end
-
-  def new_opt_out_request_notice(request_id)
-    @opt_out_request = OptOutRequest.find(request_id)
-    mail(to: OPT_OUT_REQUESTS_MANAGER, subject: 'New opt-out request')
-  end
 end
