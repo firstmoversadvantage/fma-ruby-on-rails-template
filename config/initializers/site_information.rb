3e1cf009ec00add23da9f4b93f3a9b0d8b9ec3c1
Klaudia Biala <klaudia.biala@polcode.net>
2023-01-02 17:24:25 +0100

    Update header and footer

--- config/initializers/site_information.rb
+++ config/initializers/site_information.rb
@@ -1,17 +1,22 @@
 # frozen_string_literal: true
 
+# Admin Info
 ADMIN_EMAIL = 'brian.long@fmadata.com'
 ADMIN_SITE = ''
+
+# Company Info
 COMPANY_AKA = 'FMAdata'
-FMA = 'First Movers Advantage'
-FMA_LLC = 'First Movers Advantage, LLC'
+COMPANY_NAME = 'First Movers Advantage'
+COMPANY_LLC = 'First Movers Advantage, LLC'
 OPT_OUT_REQUESTS_MANAGER = 'brian.long@fmadata.com'
-OUR_CITY_STATE_ZIP = 'Boulder, CO 80302-3812'
-OUR_COMPANY = ''
-OUR_EMAIL_ADDRESS = ''
-OUR_FAX = '(303) 443-2073'
-OUR_LOGO = ''
+
+# Contact Info
 OUR_STREET_ADDRESS = '2575 Pearl St, Ste 230'
+OUR_CITY_STATE_ZIP = 'Boulder, CO 80302-3812'
+OUR_FAX = '(303) 443-2073'
 OUR_TELEPHONE = '(303) 443-2070'
+OUR_EMAIL_ADDRESS = 'contact@fma-rails-template.com'
+
+# Website
 THIS_SITE = 'www.fma-rails-template.com'
 THIS_HOST = 'fma-rails-template.com'
